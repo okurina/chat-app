4a5d789a0a93a3c9b8248733e5fd7faa7bf1bc73
okurina <rio.60821023@gmail.com>
2023-08-25 20:45:07 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,10 +15,18 @@ class RoomsController < ApplicationController
       render :new, status: :unprocessable_entity
     end 
   end
-end
 
-private
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
+  private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids:[])
   end
+
+end
+
