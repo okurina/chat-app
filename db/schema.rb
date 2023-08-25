9c9d503fca72164b8fd0dfc474260ce8b881280c
okurina <rio.60821023@gmail.com>
2023-08-25 18:32:23 +0900

    メッセージ送信機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema[7.0].define(version: 2023_08_25_085109) do
   create_table "messages", charset: "utf8", force: :cascade do |t|
     t.string "content"
-    t.bigint "user_id", null: false
     t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
